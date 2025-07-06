62d9962a5ab0881ff4005f285d18884b8ad2810a
Samiksha712 <samikshasiva12@gmail.com>
2025-07-06 18:58:51 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Loginpage } from './pages/loginpage/loginpage';
+import { Home } from './pages/home/home';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Loginpage
+  }
+];
